06691682cca1e805998f025e44f308c1ba9567cc
FarhanRameen <103026230+FarhanRameen@users.noreply.github.com>
2022-05-31 00:13:31 +0600

    Add files via upload

---
+++ labtask1/view/registrationform.php
@@ -0,0 +1,36 @@
+<html>
+    <head>
+</head>
+<body>
+    <h1> REGISTRATION FORM </h1>
+    <form>
+
+    <table>
+        <tr><td>FIRST NAME:</td><td> <input type="text" name="fname"></td></tr>
+        <tr><td>LAST  NAME:</td><td> <input type="text" name="lname"></td></tr>
+        <tr><td>AGE:</td><td> <input type="text" name="age"></td></tr>
+
+        <tr><td>DESIGNATION:</td>
+        
+        <td><input type="radio" name="designation" value="junior programmer"> junior programmer</td>
+        <td><input type="radio" name="designation" value="senior programmer"> senior programmer</td>
+        <td><input type="radio" name="designation" value="project leader"> project leader</td></tr>
+
+        <tr><td>PREFERRED LANGUAGE:</td>
+        
+        <td><input type="checkbox" name="preferred language" value="JAVA"> JAVA</td>
+        <td><input type="checkbox" name="dpreferred language" value="PHP"> PHP</td>
+        <td><input type="checkbox" name="preferred language" value="C++"> C++</td></tr>
+        <tr><td>EMAIL:</td><td> <input type="text" name="email"></td></tr>
+        <tr><td>PASSWORD:</td><td> <input type="password" name="password"></td></tr>
+        <tr><td>please choose a file</td><td> <input type="file" id="my file" name="myfile"></td></tr>
+        <tr><td><input type="submit"> <input type="reset"></td></tr>
+
+    
+
+
+
+
+
+</body>
+</html>
